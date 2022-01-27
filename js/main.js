c8c480ccec7b092cc00e51e4f128acbaddbd39f0
simolex <simolex@yandex.ru>
2022-01-27 23:30:36 +0300

    lesson 3

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".review__slider", {
+  slidesPerView: 3,
+  loop: true,
+  speed: 400,
+  spaceBetween: 70,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+  toggle: true,
+  //   breakpoints:{}
+});
