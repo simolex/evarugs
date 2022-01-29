0af0b54dc27f32afc6878c52dcf4c8d68fe7c230
simolex <simolex@yandex.ru>
2022-01-29 15:03:12 +0300

    lesson 4

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ const swiper = new Swiper(".review__slider", {
   toggle: true,
   //   breakpoints:{}
 });
+
+const modalWindow = document.querySelector(".modal");
+const modalButton = document.querySelector(".main-display__button");
+
+modalButton.addEventListener("click", (e) => {
+  modalWindow.classList.add("active");
+});
+
+modalWindow.addEventListener("click", (e) => {
+  const isModal = e.target.closest(".modal__inner");
+  if (!isModal) {
+    modalWindow.classList.remove("active");
+  }
+});
